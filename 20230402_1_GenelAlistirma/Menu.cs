694e2f49c67ab7fb7c0c371bee8c31f13b3e84c7
ErtanOzgurCetin <ertanozgurcetin@gmail.com>
2024-01-14 20:58:37 +0300

    Ogrenci Kayıt Sistemi

---
+++ 20230402_1_GenelAlistirma/Menu.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230402_1_GenelAlistirma
+{
+    public static class Menu
+    {
+        static List<Ogrenci> ogrenciler = new List<Ogrenci>();
+        public static void Islemler(ConsoleKey key)
+        {
+            bool elemanVar = Metodlar.ListedeEleman(ogrenciler);
+            Console.Clear();
+            if (elemanVar || key == ConsoleKey.D0 || key == ConsoleKey.NumPad0 || key == ConsoleKey.NumPad1 || key == ConsoleKey.D1)
+            {
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                    case ConsoleKey.D1:
+                        Ekle("Öğrenci Ekle");   
+                        break;
+                    case ConsoleKey.NumPad2:
+                    case ConsoleKey.D2:
+                        Listele("Öğrencileri Listele");
+                        break;
+                    case ConsoleKey.NumPad3:
+                    case ConsoleKey.D3:
+                        Sil("Öğrenci Sil");
+                        break;
+                    case ConsoleKey.NumPad4:
+                    case ConsoleKey.D4:
+                        NotOrtalamasi("Öğrencilerin Genel Not Ortalaması");
+                        break;
+                    case ConsoleKey.NumPad5:
+                    case ConsoleKey.D5:
+                        YasOrtalamasi("Öğrencilerin Yaş Ortalaması");
+                        break;
+                    case ConsoleKey.NumPad6:
+                    case ConsoleKey.D6:
+                        BaslikYazdir("Toplam Öğrenci Sayısı");
+                        AnaMenuyeDon(string.Format("Toplam {0} adet öğrenci kayıtlıdır.", ogrenciler.Count));
+                        break;
+                }
+            }
+            else
+            {
+                AnaMenuyeDon("Tuşladığınız sayıya dair bir menü yok...");
+            }
+        }
+
+        private static void YasOrtalamasi(string v)
+        {
+            BaslikYazdir(v);
+            int toplamYas = 0;
+            foreach (var ogrenci in ogrenciler)
+            {
+                toplamYas += ogrenci.Yas;
+            }
+            double yasOrtalamasi = toplamYas / ogrenciler.Count;
+            AnaMenuyeDon(string.Format("{0} öğrencinin yaş ortalaması: {1}", ogrenciler.Count, Math.Round(yasOrtalamasi, 2)));
+        }
+
+        private static void NotOrtalamasi(string v)
+        {
+            BaslikYazdir(v);
+            double genelToplam = 0;
+            foreach (var ogrenci in ogrenciler)
+            {
+                genelToplam += ogrenci.Ortalama;
+            }
+            double genelNotOrtalamasi = genelToplam / ogrenciler.Count;
+            AnaMenuyeDon(string.Format("{0} adet öğrencinin genel not ortalaması: {1}", ogrenciler.Count, Math.Round(genelNotOrtalamasi, 2)));
+        }
+
+        private static void Sil(string v)
+        {
+            BaslikYazdir(v);
+            for (int i = 0; i < ogrenciler.Count; i++)
+            {
+                ogrenciler[i].Yazdir(i + 1);
+            }
+            Console.WriteLine();
+            int siraNo = Metodlar.GetInt("Silmek istediğiniz öğrencinin sıra numarasını giriniz.\nAna menüye dönmek için 0'a basınız: ", 0, ogrenciler.Count);
+            if (siraNo == 0)
+            {
+                AnaMenuyeDon("Silme işlemi iptal edildi");
+            }
+            else
+            {
+                int indexNo = siraNo- 1;
+                Console.Write(ogrenciler[indexNo].TamAd + " öğrencisini silmek istediğinize emin misiniz?(e): ");
+                if (Console.ReadKey().Key == ConsoleKey.E)
+                {
+                    string silinen = ogrenciler[indexNo].TamAd;
+                    ogrenciler.RemoveAt(indexNo);
+                    AnaMenuyeDon(string.Format("{0} isimli öğrenci başarı ile silindi", silinen));
+                }
+                else
+                {
+                    AnaMenuyeDon("Silme işlemi iptal edildi");
+                }
+            }
+        }
+
+        private static void Listele(string v)
+        {
+            BaslikYazdir(v);
+            for (int i = 0; i < ogrenciler.Count; i++)
+            {
+                ogrenciler[i].Yazdir(i + 1);
+            }
+            AnaMenuyeDon(string.Format("Toplam {0} adet öğrenci listelenmiştir.", ogrenciler.Count));
+        }
+
+        private static void Ekle(string v)
+        {
+            BaslikYazdir(v); 
+            Ogrenci o = new Ogrenci();
+            o.Ad = Metodlar.GetString("Öğrenci Adı: "); 
+            o.Soyad = Metodlar.GetString("Öğrenci Soyadı: ", 2, 15);
+            o.N1 = Metodlar.GetDouble("1. Not: ", 0,100);
+            o.N2 = Metodlar.GetDouble("2. Not: ", 0, 100);
+            o.DogumTarihi = Metodlar.GetDateTime("Doğum Tarihi: ", 1973, 2013);
+            ogrenciler.Add(o);
+            AnaMenuyeDon(string.Format("{0} öğrencisi başarı ile listeye eklendi", o.TamAd));
+        }
+
+        private static void AnaMenuyeDon(string v)
+        {
+            Console.WriteLine();
+            Console.WriteLine(v);
+            Console.WriteLine("Ana menüye dönmek için bir tuşa basınız");
+            Console.ReadKey();
+        }
+
+        private static void BaslikYazdir(string v)
+        {
+            Console.WriteLine(v);
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 20230402_1_GenelAlistirma/Metodlar.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230402_1_GenelAlistirma
+{
+    public static class Metodlar
+    {
+        public static string GetString(string metin, int min = 2, int max = 20)
+        {
+            string txt = string.Empty;
+            bool hata = false;
+            do
+            {
+                Console.Write(metin);
+                txt = Console.ReadLine();
+                if (string.IsNullOrEmpty(txt))
+                {
+                    Console.WriteLine("Boş bir değer giremezsiniz");
+                    hata = true;
+                }
+                else
+                {
+                    if (txt.Length < min || txt.Length > max)
+                    {
+                        Console.WriteLine("Lütfen min. {0}, max. {1} uzunlukta metin giriniz",min,max);
+                        hata = true;
+                    }
+                    else
+                    {
+                        int sayac = 0;
+                        foreach (var item in txt) // Ahm3t
+                        {
+                            try
+                            {
+                                int.Parse(Char.ToString(item));
+                                // Convert.ToInt32(Char.ToString(item));
+                                sayac++;
+                            }
+                            catch
+                            {
+                            }
+                        }
+                        if (sayac > 0)
+                        {
+                            Console.WriteLine("Lütfen sayısal bir değer kullanmayınız");
+                            hata = true;
+                        }
+                        else
+                        {
+                            hata = false;
+                        }
+                    }
+                }
+            } while (hata);
+            return txt; 
+        }
+
+        
+        public static int GetInt(string metin, int min = int.MinValue, int max = int.MaxValue)
+        {
+            int sayi = 0;
+            bool hata = false;
+            do
+            {
+                Console.Write(metin);
+                try
+                {
+                    sayi = int.Parse(Console.ReadLine());
+                    if (sayi >= min && sayi <= max)
+                    {
+                        hata = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen {0} ile {1} arasında bir değer giriniz", min,max);
+                        hata = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    hata = true;
+                }
+            } while (hata);
+            return sayi;
+        }
+
+        public static double GetDouble(string metin, double min = double.MinValue, double max = double.MaxValue)
+        {
+            double sayi = 0;
+            bool hata = false;
+            do
+            {
+                Console.Write(metin);
+                try
+                {
+                    sayi = double.Parse(Console.ReadLine());
+                    if (sayi >= min && sayi <= max)
+                    {
+                        hata = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen {0} ile {1} arasında bir değer giriniz", min,max);
+                        hata = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    hata = true;
+                }
+            } while (hata);
+            return sayi;
+        }
+
+        public static DateTime GetDateTime (string metin, int minYear, int maxYear)
+        {
+            DateTime date = DateTime.Now;
+            bool hata = false;
+            do
+            {
+                Console.Write(metin); 
+                try
+                {
+                    date = DateTime.Parse(Console.ReadLine());
+                    if (date.Year <= maxYear && date.Year >= minYear)
+                    {
+                        hata = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen {0} ile {1} yılları arasında bir tarih giriniz", minYear,maxYear);
+                        hata = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    hata = true;
+                }
+            } while (hata);
+            return date;
+        }
+
+        public static bool ListedeEleman(List<Ogrenci> ogrenci)
+        {
+            // Koleksiyonlar içerisinde bulunan Any() metodu koleksiyon içerisinde öğe var mı? yok mu? sorgulamasını yapar. Geriye bool tipinde değer döndürür. Var ise "true" yok ise "false" değeri döner.
+            if (ogrenci.Any())
+                return true;
+            else
+                return false;
+        }
+    }
+}
